7f4b646afa4937979f68d8dffbf1fd2fef8346e4
James Koster <james@jameskoster.co.uk>
2012-05-18 11:52:39 +0100

    Added WP link in footer credit

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer id="footer" class="row">
 	
 	<p>
-		<?php _e('Designed by ','koster'); ?><a href="http://jameskoster.co.uk/">James Koster</a>.
+		<?php _e('Powered by','koster'); ?> <a href="http://wordpress.org" title="WordPress.org">WordPress</a>, <?php _e('Designed by','koster'); ?> <a href="http://jameskoster.co.uk/">James Koster</a>.
 	</p>
 
 </footer>
